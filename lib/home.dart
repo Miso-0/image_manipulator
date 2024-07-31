a3cd41dada4d0a5b524c3d0f97da674482623c8b
Miso Menze <72578071+Miso-0@users.noreply.github.com>
2024-07-31 18:25:43 +0200

    Batman

---
+++ lib/home.dart
@@ -0,0 +1,30 @@
+// import 'package:flutter/material.dart';
+
+// class Home extends StatefulWidget {
+//   const Home({
+//     super.key,
+//   });
+
+//   @override
+//   State<Home> createState() => _HomeState();
+// }
+
+// class _HomeState extends State<Home> {
+//   @override
+//   void initState() {
+//     super.initState();
+
+
+// final image = AssetImage('');
+// image.loadImage(key, decode)
+    
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: Text('Hello World!'),
+//       ),
+//     );
+//   }
+// }
---
+++ lib/main_app.dart
@@ -0,0 +1,13 @@
+// import 'package:flutter/material.dart';
+// import 'package:image_manipulator/home.dart';
+
+// class MainApp extends StatelessWidget {
+//   const MainApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Home(),
+//     );
+//   }
+// }
